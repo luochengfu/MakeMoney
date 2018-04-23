2987db42c4babe4c36c583554593482a4cc90738
张鹏\ZhangPeng <p.zhang@doubozhibo.com>
2018-04-23 23:19:19 +0800

    添加第三方图片筛选器

--- app/src/main/java/com/tudouni/makemoney/myApplication/MyApplication.java
+++ app/src/main/java/com/tudouni/makemoney/myApplication/MyApplication.java
@@ -2,11 +2,9 @@ package com.tudouni.makemoney.myApplication;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.IntentFilter;
-import android.support.v4.content.LocalBroadcastManager;
-import android.text.TextUtils;
 
-import com.google.gson.Gson;
+import com.lzy.imagepicker.ImagePicker;
+import com.lzy.imagepicker.view.CropImageView;
 import com.tudouni.makemoney.model.AppConfig;
 import com.tudouni.makemoney.model.User;
 import com.tudouni.makemoney.network.CommonScene;
@@ -20,6 +18,7 @@ import com.umeng.commonsdk.UMConfigure;
 import com.umeng.socialize.PlatformConfig;
 import com.umeng.socialize.UMShareAPI;
 import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.uuzuche.lib_zxing.PicassoImageLoader;
 import com.uuzuche.lib_zxing.activity.ZXingLibrary;
 
 import java.io.UnsupportedEncodingException;
@@ -58,6 +57,18 @@ public class MyApplication extends BaseApplication {
         initUmeng();
         ZXingLibrary.initDisplayOpinion(this);
         registerActivityLifecycleCallbacks();
+
+        ImagePicker imagePicker = ImagePicker.getInstance();
+        imagePicker.setImageLoader(new PicassoImageLoader());   //设置图片加载器
+        imagePicker.setShowCamera(true);  //显示拍照按钮
+        imagePicker.setCrop(true);        //允许裁剪（单选才有效）
+        imagePicker.setSaveRectangle(true); //是否按矩形区域保存
+        imagePicker.setSelectLimit(9);    //选中数量限制
+        imagePicker.setStyle(CropImageView.Style.RECTANGLE);  //裁剪框的形状
+        imagePicker.setFocusWidth(800);   //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setFocusHeight(800);  //裁剪框的高度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setOutPutX(1000);//保存文件的宽度。单位像素
+        imagePicker.setOutPutY(1000);//保存文件的高度。单位像素
     }
 
     private void initConfig() {
@@ -100,7 +111,7 @@ public class MyApplication extends BaseApplication {
     }
 
     public static void saveLoginUser(User user) {
-        if(null == user)
+        if (null == user)
             return;
         mLoginUser = user;
         UserInfoHelper.saveUserDatas(sContext, user);
