370d9df7ddfb874f461f86ca4a8f6fd671cae53d
Jaron.Wu <33626326@qq.com>
2018-04-24 13:22:12 +0800

    调整品类UI

--- app/src/main/java/com/tudouni/makemoney/viewModel/GoodCategoryViewModel.java
+++ app/src/main/java/com/tudouni/makemoney/viewModel/GoodCategoryViewModel.java
@@ -1,25 +1,29 @@
 package com.tudouni.makemoney.viewModel;
 
-import com.tudouni.makemoney.fragment.category.GoodCategoryFragment;
 import com.tudouni.makemoney.model.Category;
 import com.tudouni.makemoney.network.CommonScene;
 import com.tudouni.makemoney.network.rx.BaseObserver;
 import com.tudouni.makemoney.utils.TDLog;
 
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 品类ViewModel
+ * Jaron.Wu
+ *      2018/04/23
+ */
 public class GoodCategoryViewModel extends LoadingViewModel {
 
-    private List<Category> data = new ArrayList<>();
 
-    public void getGoodList(final GoodCategoryFragment.ResultCallback callback){
+    public void getGoodList(final VMResultCallback<List<Category>> callback){
         CommonScene.getGoodList(new BaseObserver<List<Category>>() {
             @Override
             public void OnSuccess(List<Category> category) {
                 if (category != null) {
-                    data.clear();
-                    data.addAll(category);
+                    if (category.size() > 0) {
+                        //默认选中第一个品类
+                        category.get(0).setSelected(true);
+                    }
                 }
                 if (callback != null) {
                     callback.onSuccess(category);
