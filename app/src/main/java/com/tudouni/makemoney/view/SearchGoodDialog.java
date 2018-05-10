0a3c6fd519a004e8af14da653a7ed70e3c381a4f
莫达军 <1007846591@qq.com>
2018-05-10 12:36:23 +0800

    搜索弹框缓存

--- app/src/main/java/com/tudouni/makemoney/view/SearchGoodDialog.java
+++ app/src/main/java/com/tudouni/makemoney/view/SearchGoodDialog.java
@@ -11,6 +11,10 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.tudouni.makemoney.R;
 import com.tudouni.makemoney.activity.H5Activity;
 import com.tudouni.makemoney.activity.WebvewRefreshActivity;
@@ -18,9 +22,14 @@ import com.tudouni.makemoney.activity.search.SearchActivity;
 import com.tudouni.makemoney.myApplication.MyApplication;
 import com.tudouni.makemoney.network.NetConfig;
 import com.tudouni.makemoney.utils.ClipboardUtil;
+import com.tudouni.makemoney.utils.SPUtil;
 import com.tudouni.makemoney.utils.ScreenUtils;
+import com.tudouni.makemoney.utils.StringUtil;
+import com.tudouni.makemoney.utils.TDLog;
 
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/5/2 0002.
@@ -33,6 +42,8 @@ public class SearchGoodDialog extends Dialog implements View.OnClickListener
     private RelativeLayout imgLayout;
     private TextView tvMsg, mNoButton, mYesButton;
     private String searchStr;
+    private Gson mGson;
+    private List<String> mSearchhistory;
 
     public SearchGoodDialog(Context context) {
         this(context, null, true);
@@ -95,6 +106,7 @@ public class SearchGoodDialog extends Dialog implements View.OnClickListener
                 break;
             case R.id.tvYes:
                 try {
+                    saveSearchHistory(searchStr);
                     String url = NetConfig.getBaseTuDouNiH5Url() + "html/resultlist.html?unionid=" + MyApplication.getLoginUser().getUnionid()+"&search=" + URLEncoder.encode(searchStr,"utf-8").replaceAll("\\+",  "%20");
                     Intent intent = new Intent(mContext, H5Activity.class);
                     intent.putExtra("titleStatus",1);
@@ -113,6 +125,37 @@ public class SearchGoodDialog extends Dialog implements View.OnClickListener
         super.dismiss();
         ClipboardUtil.getInstance().copyText("","");
         MyApplication.mClipStr = "";
+    }
+
+    /**
+     * 保存搜索记录
+     */
+    private void saveSearchHistory(String search) {
+        if(mGson == null)
+            mGson = new Gson();
+
+        if(mSearchhistory == null)
+            mSearchhistory = new ArrayList<>();
+
+        String searchHistoryJson = SPUtil.getString(mContext, "search_history");
+        if (searchHistoryJson == null || StringUtil.isEmpty(searchHistoryJson)) {
+            mSearchhistory.add(search);
+            SPUtil.putString(mContext, "search_history", mGson.toJson(mSearchhistory));
+            return;
+        }
+        //查看是否存在
+        JsonParser parser = new JsonParser();
+        JsonArray searchArr = parser.parse(searchHistoryJson).getAsJsonArray();
+        for (JsonElement item : searchArr) {
+            String itemStr = mGson.fromJson(item, String.class);
+            if(search.equals(itemStr))
+                return;
+            mSearchhistory.add(itemStr);
+        }
+
+        mSearchhistory.add(search);
+        SPUtil.putString(mContext, "search_history", mGson.toJson(mSearchhistory));
+
     }
 
 }
