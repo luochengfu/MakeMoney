0874a91711d2f357a6ea6f3b0e9814a12b2691fa
JaronWu <jaronwu@q.com>
2018-05-19 08:48:03 +0800

    我赚的钱订单时间问题

--- app/src/main/java/com/tudouni/makemoney/viewModel/MyEarningsViewModel.java
+++ app/src/main/java/com/tudouni/makemoney/viewModel/MyEarningsViewModel.java
@@ -16,7 +16,10 @@ import com.tudouni.makemoney.network.NetConfig;
 import com.tudouni.makemoney.network.rx.BaseObserver;
 import com.tudouni.makemoney.utils.TDLog;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class MyEarningsViewModel extends LoadingViewModel {
@@ -28,16 +31,30 @@ public class MyEarningsViewModel extends LoadingViewModel {
         CommonScene.loadIncomeProfile(MyApplication.getLoginUser().getUid(), new BaseObserver<MyEarnings>() {
             @Override
             public void OnSuccess(MyEarnings myEarnings) {
-
-
                 Calendar calendar1 = Calendar.getInstance();
+                int currDayOfMonth = calendar1.get(Calendar.DAY_OF_MONTH);
+                int currMonth = calendar1.get(Calendar.MONTH);
+                int currYear = calendar1.get(Calendar.YEAR);
                 calendar1.set(calendar1.get(Calendar.YEAR), calendar1.get(Calendar.MONTH), calendar1.get(Calendar.DAY_OF_MONTH),
                         0, 0, 0);
-                long todayStart = calendar1.getTimeInMillis()/1000;
-                long currentSecond = System.currentTimeMillis()/1000;
-                TDLog.e(calendar1.getTime(),todayStart,currentSecond);
+                long todayStart = calendar1.getTimeInMillis() / 1000;
+                long currentSecond = System.currentTimeMillis() / 1000;
+                TDLog.e(calendar1.getTime(), todayStart, currentSecond, currDayOfMonth, currMonth);
+                long yesterdayStart = todayStart - 24 * 60 * 60;
+                long yesterdayEnd = todayStart - 1;
+                long currMonthStart = todayStart - (currDayOfMonth - 1) * 24 * 60 * 60;
+                long currMonthEnd = currentSecond;
+                if (currMonth > 0) {
+                    calendar1.set(Calendar.MONTH, currMonth - 1);
+                } else {
+                    calendar1.set(Calendar.MONTH, 11);
+                    calendar1.set(Calendar.YEAR, currYear - 1);
+                }
+                int maxDayOfThisMonth = calendar1.getActualMaximum(Calendar.DATE);
+                long lastMonthStart = currMonthStart - maxDayOfThisMonth * 24 * 60 * 60;
+                long lastMonthEnd = currMonthStart - 1;
 
-
+                TDLog.e(maxDayOfThisMonth, transForDate(todayStart), transForDate(currentSecond), transForDate(yesterdayStart), transForDate(yesterdayEnd), transForDate(currMonthStart), transForDate(currMonthEnd), transForDate(lastMonthStart), transForDate(lastMonthEnd));
 
 
                 EarningsBean todayEarnings = new EarningsBean(myEarnings.getTodayIncome(), myEarnings.getTodayCount(), myEarnings.getTodayExpectedIncome(), EarningsBean.IncomeType.TYPE_TODAY);
@@ -45,10 +62,16 @@ public class MyEarningsViewModel extends LoadingViewModel {
                 todayEarnings.setEndTime(currentSecond);
 
                 EarningsBean yesterdayEarnings = new EarningsBean(myEarnings.getYesterdayIncome(), myEarnings.getYesterdayCount(), myEarnings.getYesterdayExpectedIncome(), EarningsBean.IncomeType.TYPE_YESTERDAY);
+                yesterdayEarnings.setStartTime(yesterdayStart);
+                yesterdayEarnings.setEndTime(yesterdayEnd);
 
                 EarningsBean thisMonthEarnings = new EarningsBean(myEarnings.getLastMonthIncome(), myEarnings.getLastMonthCount(), myEarnings.getLastMonthExpectedIncome(), EarningsBean.IncomeType.TYPE_THIS_MONTH);
+                thisMonthEarnings.setStartTime(currMonthStart);
+                thisMonthEarnings.setEndTime(currMonthEnd);
 
                 EarningsBean lastMonthEarnings = new EarningsBean(myEarnings.getLastMonthIncome(), myEarnings.getLastMonthCount(), myEarnings.getLastMonthExpectedIncome(), EarningsBean.IncomeType.TYPE_LAST_MONTH);
+                lastMonthEarnings.setStartTime(lastMonthStart);
+                lastMonthEarnings.setEndTime(lastMonthEnd);
 
                 earnings.set(myEarnings);
                 if (callback != null) {
@@ -58,6 +81,20 @@ public class MyEarningsViewModel extends LoadingViewModel {
 
             }
         });
+    }
+
+
+    public Date transForDate(long ms) {
+        long msl = ms * 1000;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date temp = null;
+        try {
+            String str = sdf.format(msl);
+            temp = sdf.parse(str);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return temp;
     }
 
     public void loadRankData(VMResultCallback<List<EarningsRank>> callback) {
@@ -71,11 +108,11 @@ public class MyEarningsViewModel extends LoadingViewModel {
         });
     }
 
-    public double getBalance(){
+    public double getBalance() {
         return earnings.get().getBalance();
     }
 
-    public void finish(){
+    public void finish() {
         Activity currActivity = MyApplication.sCurrActivity;
         if (currActivity != null) {
             currActivity.finish();
