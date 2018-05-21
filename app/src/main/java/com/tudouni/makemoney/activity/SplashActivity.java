41771805ab7349bda6f8b52a2b31e9bf8cb67b4d
张鹏\ZhangPeng <p.zhang@doubozhibo.com>
2018-05-21 22:48:39 +0800

    启动界面添加权限

--- app/src/main/java/com/tudouni/makemoney/activity/SplashActivity.java
+++ app/src/main/java/com/tudouni/makemoney/activity/SplashActivity.java
@@ -1,24 +1,44 @@
 package com.tudouni.makemoney.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.text.TextUtils;
 
 import com.tudouni.makemoney.R;
 import com.tudouni.makemoney.myApplication.MyApplication;
 import com.tudouni.makemoney.utils.CommonUtil;
 import com.umeng.analytics.MobclickAgent;
+import com.umeng.socialize.UMShareAPI;
 
 
 public class SplashActivity extends Activity {
-
+    private final int PERMISSION_REQUEST_CODE = 124;
     private Context mContext;
     private Handler mHandler = new Handler();
     private int WAIT_TIME = 2; //单位秒
+    private final String[] permissionManifest = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+    private final int[] noPermissionTip = {
+            R.string.no_camera_permission,
+            R.string.no_record_audio_permission,
+            R.string.no_read_phone_state_permission,
+            R.string.no_write_external_storage_permission,
+            R.string.no_read_external_storage_permission
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,7 @@ public class SplashActivity extends Activity {
             return;
         }
         setContentView(R.layout.activity_splash);
+        initPermissions();
         setTitle("启动页");
         mContext = this;
         initView();
@@ -88,4 +109,42 @@ public class SplashActivity extends Activity {
         super.onPause();
         MobclickAgent.onPause(this);
     }
+
+    /**
+     * 初始化权限
+     */
+    private void initPermissions() {
+        if (!permissionCheck()) {
+            if (Build.VERSION.SDK_INT >= 23) {
+                ActivityCompat.requestPermissions(this, permissionManifest,
+                        PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    /**
+     * 权限检查（适配6.0以上手机）
+     */
+    private boolean permissionCheck() {
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        String permission;
+        for (int i = 0; i < permissionManifest.length; i++) {
+            permission = permissionManifest[i];
+            if (PermissionChecker.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                permissionCheck = PackageManager.PERMISSION_DENIED;
+            }
+        }
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
